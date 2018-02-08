86f3df48722c2d503c89c8465b7ed43b7c1a3d22
Konstantin Simeonove <konstantin.simeonov@chaosgroup.com>
2018-02-08 16:40:17 +0200

    Implemented pending tests for repeat strategy

--- lib/__tests__/repeat-last-action-strategy.spec.ts
+++ lib/__tests__/repeat-last-action-strategy.spec.ts
@@ -110,10 +110,139 @@ test(`returns set containing old data minus every data from the range [start, en
 	assert.deepEqual([...newSelection], expectedSelection);
 });
 
-test('behaves as called with currentActionIndex: 0 when currentActionIndex is negative', assert => { });
-test('behaves as called with children.length - 1 when currentActionIndex is greater then children.length', assert => { });
-test('behaves as called with lastActionIndex: 0 when lastActionIndex is negative', assert => { });
-test('behaves as called with children.length - 1 when lastActionIndex is greater then children.length', assert => { });
+test('behaves as called with currentActionIndex: 0 when currentActionIndex is negative', assert => {
+	const start = 2;
+	const end = 0;
+	const expectedSelection = [
+		'public static void Main() {',
+		'	Console.WriteLine("iskaplache");',
+		'}',
+	];
+
+	const initialSelection = [
+		'class Program1 {',
+		...expectedSelection,
+	];
+	
+	const allChildrenData = [
+		'using System;',
+		'using System.Collection.Generics;',
+		...initialSelection,
+		'}'
+	];
+
+	const selectionContext = selectionCtx({
+		...minSelectionContext,
+		lastActionIndex: start,
+		currentActionIndex: -1,
+		data: allChildrenData[end],
+		lastAction: ('delete' as SelectionAction),
+		selection: new Set(initialSelection),
+		children: toMockSelectables(allChildrenData)
+	});
+
+	const newSelection = repeatLastSelectionAction.getNewSelection(selectionContext);
+	assert.is(newSelection.size, expectedSelection.length);
+	assert.deepEqual([...newSelection], expectedSelection);
+});
+
+test('behaves as called with children.length - 1 when currentActionIndex is greater then children.length', assert => {
+	const start = 1;
+
+	const text = `
+	main :: IO ()
+	main = do
+		args <- getArgs
+		let filePath = head args
+		let command  = args !! 1
+		fullPath <- canonicalizePath filePath
+		putStrLn $ "Watching file " ++ fullPath
+		ignoreError $ runCmd command fullPath
+		lastTimeModified <- getModificationTime fullPath
+		setInterval 1000 (\time -> do
+				newTime <- getModificationTime filePath
+				if time < newTime
+					then ignoreError $ runCmd command fullPath
+					else return () :: IO ()
+	return newTime) lastTimeModified
+`.split('\n');
+	
+	const initialSelection = text.slice(start, 4);
+	const expectedSelection = text.slice(start);
+	
+	const selectionContext = selectionCtx({
+		...minSelectionContext,
+		lastActionIndex: start,
+		currentActionIndex: text.length + 3,
+		data: text[start],
+		lastAction: ('add' as SelectionAction),
+		selection: new Set(initialSelection),
+		children: toMockSelectables(text)
+	});
+
+	const newSelection = repeatLastSelectionAction.getNewSelection(selectionContext);
+	assert.is(newSelection.size, expectedSelection.length);
+	assert.deepEqual([...newSelection], expectedSelection);
+});
+
+test('behaves as called with lastActionIndex: 0 when lastActionIndex is negative', assert => {
+	const text = `
+	let rec sort = function
+		| [] -> []
+		| x :: l -> insert x (sort l)
+	and insert elem = function
+		| [] -> [elem]
+		| x :: l -> if elem < x then elem :: x :: l
+`.split('\n');
+
+	const end = 4;
+	const initialSelection = text.slice(0, end + 2);
+	const expectedSelection = text.slice(end, end + 2);
+
+	const selectionContext = selectionCtx({
+		...minSelectionContext,
+		lastActionIndex: -42,
+		currentActionIndex: end - 1,
+		data: text[end],
+		lastAction: ('delete' as SelectionAction),
+		selection: new Set(initialSelection),
+		children: toMockSelectables(text)
+	});
+
+	const newSelection = repeatLastSelectionAction.getNewSelection(selectionContext);
+	assert.is(newSelection.size, expectedSelection.length);
+	assert.deepEqual([...newSelection], expectedSelection);
+});
+
+
+test('behaves as called with children.length - 1 when lastActionIndex is greater then children.length', assert => {
+	const text = `
+	func Reverse(s string) string {
+		r := []rune(s)
+		for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+			r[i], r[j] = r[j], r[i]
+		}
+		return string(r)
+	}
+`.split('\n');
+
+	const initialSelection = [];
+	const expectedSelection = text.slice(2);
+
+	const selectionContext = selectionCtx({
+		...minSelectionContext,
+		lastActionIndex: 1000,
+		currentActionIndex: 2,
+		data: text[2],
+		lastAction: ('add' as SelectionAction),
+		selection: new Set(initialSelection),
+		children: toMockSelectables(text)
+	});
+
+	const newSelection = repeatLastSelectionAction.getNewSelection(selectionContext);
+	assert.is(newSelection.size, expectedSelection.length);
+	assert.deepEqual([...newSelection], expectedSelection);
+});
 
 {
 	const selectionContext = selectionCtx({
