8cfbe7bd724a6f6356e9bb82c4331d37fd6df23c
Konstantin Simeonov <konstantin.simeonov@chaosgroup.com>
2018-03-22 14:32:11 +0200

    Use navigator.platform to determine os

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,10 @@
 import { OSName } from './constants';
 export const ensureRange = (min: number, value: number, max: number) => Math.max(min, Math.min(value, max));
 
-export const _getOsNameFromUserAgent = (userAgent: string): OSName => [
-	{ name: OSName.MAC, test: (agentString: string) => /mac/i.test(agentString) },
-	{ name: OSName.OTHER, test: (agentString: string) => true }
-].find(({ test }) => test(userAgent)).name;
+export const _getOsNameFromUserAgent = (platform: string): OSName => [
+	{ name: OSName.MAC, test: (platformString: string) => /mac/i.test(platformString) },
+	{ name: OSName.OTHER, test: (platformString: string) => true }
+].find(({ test }) => test(platform)).name;
 
-const osName = _getOsNameFromUserAgent(window.navigator.userAgent);
+const osName = _getOsNameFromUserAgent(window.navigator.platform);
 export const getOsName = () => osName;
