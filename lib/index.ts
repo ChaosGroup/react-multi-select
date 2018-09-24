c7886309a8bcba4efa97dcb90ec743f68360192f
Konstantin Simeonov <konstantin.simeonov@chaosgroup.com>
2018-09-24 20:07:03 +0300

    Do not pass onSelectionChange through

--- lib/index.ts
+++ lib/index.ts
@@ -152,7 +152,7 @@ export default class MultiSelect<DT> extends React.PureComponent<TMultiSelectPro
 	private _onClick = () => this._walker.currentNode = document.activeElement;
 
 	public render() {
-		const { render, children, selection, manageFocus, ...rest } = this.props;
+		const { render, children, selection, manageFocus, onSelectionChange, ...rest } = this.props;
 		const childrenWithPassedProps = React.Children.map(
 			children,
 			(childElement: React.ReactElement<TSelectableProps<DT>>, index: number) => {
