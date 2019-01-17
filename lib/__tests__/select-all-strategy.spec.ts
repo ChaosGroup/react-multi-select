390e103b0b5f54c4fa364c9df6b91869c1bca394
Konstantin Simeonov <konstantin.simeonov@chaosgroup.com>
2019-01-17 08:50:02 +0200

    Rewrite selectAll strategy tests with fast-check

--- lib/__tests__/select-all-strategy.spec.ts
+++ lib/__tests__/select-all-strategy.spec.ts
@@ -2,33 +2,32 @@ import './helpers/browser';
 import test from 'ava';
 import * as fc from 'fast-check';
 
-import { minSelectionContext, selectionCtx } from './helpers/index';
 import { SelectionAction, SelectionType } from '../handle-selection/types';
 import * as selectAll from '../handle-selection/select-all-strategy';
 
+const arbitrarySelectionContext = <T>(arbitrary: fc.Arbitrary<T>, minE: number = 1, maxE: number = 1000) =>
+	fc.array(arbitrary, minE, maxE)
+		.chain(childrenData => fc.tuple(fc.constant(childrenData), fc.shuffledSubarray(childrenData)))
+		.chain(([data, selection]) => fc.record({
+			selection: fc.constant(new Set(selection)),
+			data: fc.constantFrom(...data),
+			lastAction: fc.constantFrom('add' as SelectionAction, 'remove' as SelectionAction),
+			lastActionIndex: fc.integer(0, data.length),
+			currentActionIndex: fc.integer(0, data.length),
+			childrenData: fc.constant(data),
+			selectionType: fc.constant('keyboard' as SelectionType),
+			altKey: fc.boolean(),
+			ctrlKey: fc.boolean(),
+			shiftKey: fc.boolean(),
+			key: fc.constantFrom(
+				...Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97))
+			)
+		}));
 {
 	test('matches ctrl+a and nothing else', assert => {
-		const arbitrarySelectionContext = fc.array(fc.integer(), 1, 1000)
-			.chain(childrenData => fc.tuple(fc.constant(childrenData), fc.shuffledSubarray(childrenData)))
-			.chain(([data, selection]) => fc.record({
-				selection: fc.constant(new Set(selection)),
-				data: fc.constantFrom(...data),
-				lastAction: fc.constantFrom('add' as SelectionAction, 'remove' as SelectionAction),
-				lastActionIndex: fc.integer(0, data.length),
-				currentActionIndex: fc.integer(0, data.length),
-				childrenData: fc.constant(data),
-				selectionType: fc.constant('keyboard' as SelectionType),
-				altKey: fc.boolean(),
-				ctrlKey: fc.boolean(),
-				shiftKey: fc.boolean(),
-				key: fc.constantFrom(
-					...Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97))
-				)
-			}));
-
 		fc.assert(
 			fc.property(
-				arbitrarySelectionContext,
+				arbitrarySelectionContext(fc.integer()),
 				ctx => selectAll.matches.keyboard(ctx) === (ctx.ctrlKey && ctx.key === 'a')
 			)
 		);
@@ -40,38 +39,38 @@ test(`doesn't have "mouse" property`, assert => {
 	assert.false('mouse' in selectAll.matches);
 });
 
-test('returns a Set containing the same data when selected children is an empty array', assert => {
-	const selectionContext = selectionCtx<number>({ childrenData: [] });
+test('getNewSelection returns all elements of childrenData in a Set', assert => {
+	const selectionContextArb = arbitrarySelectionContext(fc.string());
 
-	const expectedSelection = selectionContext.selection;
-	const newSelection = selectAll.getNewSelection(selectionContext);
+	fc.assert(
+		fc.property(
+			selectionContextArb,
+			ctx => {
+				const newSelection = selectAll.getNewSelection(ctx);
+				const expectedSelection = new Set(ctx.childrenData);
+				if (newSelection.size !== expectedSelection.size) {
+					return false;
+				}
 
-	assert.is(newSelection.size, expectedSelection.size);
-	assert.deepEqual([...newSelection], [...expectedSelection]);
+				for (const v of newSelection) {
+					if (!expectedSelection.has(v)) {
+						return false;
+					}
+				}
+
+				return true;
+			}
+		)
+	);
+	assert.pass();
 });
 
-test('returns a new Set containing the data from the set and the data from the children', assert => {
-	const initiallySelected = [1, 5, 'beep boop'];
-	const childrenData = [7, 5, 'beep'];
-	const selectionContext = selectionCtx({
-		...minSelectionContext,
-		selection: new Set(initiallySelected),
-		childrenData
-	});
-
-	const expectedSelection = new Set([...initiallySelected, ...childrenData]);
-	const newSelection = selectAll.getNewSelection(selectionContext);
-	assert.deepEqual([...newSelection], [...expectedSelection]);
-});
-
-test('returns empty Set for empty children array and empty prior selection', assert => {
-	const selectionContext = selectionCtx<number>({ selection: new Set, childrenData: [] });
-
-	const newSelection = selectAll.getNewSelection(selectionContext);
-
-	assert.is(newSelection.size, 0);
-});
-
-test('returns null', assert => {
-	assert.is(selectAll.getStateUpdates(minSelectionContext), null);
+test('getStateUpdates always returns null updates', assert => {
+	fc.assert(
+		fc.property(
+			arbitrarySelectionContext(fc.integer()),
+			ctx => selectAll.getStateUpdates(ctx) === null
+		)
+	);
+	assert.pass();
 });
